ee620efb58acf988bd999e08e53f62a2e0ba84d4
fabiohanada23 <61828534+fabiohanada23@users.noreply.github.com>
2021-10-17 22:32:18 -0300

    Add files via upload

---
+++ menu.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Menu</title>
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+</head>
+<body>
+
+	<div class="container" style="margin-top: 100px">
+<div style="text-align: right;">
+			<a href="../usuario/index.php" role="button" class="btn btn-primary">Voltar</a>
+		</div>
+		<div class="row">
+			<div class="col-sm-6">
+				<div class="card">
+					<div class="card-body">
+						<h5 class="card-title">Adicionar Livro</h5>
+						<p class="card-text">Opção para adicionar Livro em nosso estoque.</p>
+						<a href="adicionar_livro.php" class="btn btn-primary">Cadastrar Livro</a>
+					</div>
+				</div>
+			</div>
+			<div class="col-sm-6">
+				<div class="card">
+					<div class="card-body">
+						<h5 class="card-title">Listar Livros</h5>
+						<p class="card-text">Visualizar, editar e excluir os Livros.</p>
+						<a href="listar_livros.php" class="btn btn-primary">Livros</a>
+					</div>
+				</div>
+			</div>
+		</div>
+
+	</div>
+
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+</body>
+</html>
